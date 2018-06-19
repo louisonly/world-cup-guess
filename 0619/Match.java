5f16b5b15b509ef9d22dbd5d08de92606b9173b8
louisonly <louisonly@163.com>
2018-06-19 20:54:09 +0800

    Create Match.java

---
+++ 0619/Match.java
@@ -0,0 +1,95 @@
+package com.louis.entity;
+
+import java.util.Date;
+
+
+public class Match {
+
+	private int matchId;
+	private Date matchDate;
+	//private Timestamp matchDate;
+	private String home;
+	private String away;
+	private double homeOdds;
+	private double drawOdds;
+	private double awayOdds;
+	private Date guessDeadline;
+	//private Timestamp guessDeadline;
+	private String score;
+	private int start;
+	private int end;
+	private String resultType;
+	public int getMatchId() {
+		return matchId;
+	}
+	public void setMatchId(int matchId) {
+		this.matchId = matchId;
+	}
+	public Date getMatchDate() {
+		return matchDate;
+	}
+	public void setMatchDate(Date matchDate) {
+		this.matchDate = matchDate;
+	}
+	public String getHome() {
+		return home;
+	}
+	public void setHome(String home) {
+		this.home = home;
+	}
+	public String getAway() {
+		return away;
+	}
+	public void setAway(String away) {
+		this.away = away;
+	}
+	public double getHomeOdds() {
+		return homeOdds;
+	}
+	public void setHomeOdds(double homeOdds) {
+		this.homeOdds = homeOdds;
+	}
+	public double getDrawOdds() {
+		return drawOdds;
+	}
+	public void setDrawOdds(double drawOdds) {
+		this.drawOdds = drawOdds;
+	}
+	public double getAwayOdds() {
+		return awayOdds;
+	}
+	public void setAwayOdds(double awayOdds) {
+		this.awayOdds = awayOdds;
+	}
+	public Date getGuessDeadline() {
+		return guessDeadline;
+	}
+	public void setGuessDeadline(Date guessDeadline) {
+		this.guessDeadline = guessDeadline;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public String getResultType() {
+		return resultType;
+	}
+	public void setResultType(String resultType) {
+		this.resultType = resultType;
+	}
+}
+
